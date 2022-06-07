93d13015c7437c3f4f411a5207f3029caef3ec54
Deepak <aroradeepak485@gmail.com>
2022-06-07 14:39:12 +0530

    useReducer

--- src/PhoneDirectory.js
+++ src/PhoneDirectory.js
@@ -1,17 +1,21 @@
-import React, { useCallback, useEffect, useMemo, useState } from "react";
+import React, { useCallback, useEffect, useMemo, useReducer, useState } from "react";
 import AddSubscriber from "./AddSubscriber";
 import ShowSubscribers from "./ShowSubscribers";
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Footer from "./Footer";
 import {SubscriberCountContext} from "./SubscriberCountContext";
+import { TotalSubscribersReducer } from "./TotalSubscribersReducer";
 
 export default function PhoneDirectory() {
 
     const [subscribersList, setSubscribersList] = useState([]);
 
+      const [state, dispatch] = useReducer(TotalSubscribersReducer, {count : 0})
+
     async function loadData(){
         const rawResponse = await fetch("http://localhost:7081/api/contacts")
         const data = await rawResponse.json()
+        dispatch({"type": "UPDATE_COUNT", payload: data.length})
         setSubscribersList(data);
     }
     useEffect(()=>{
@@ -24,18 +28,7 @@ export default function PhoneDirectory() {
         const data = await rawResponse.json();
         loadData();
     }, [subscribersList])
-    // async function deleteSubscriberHandler(subscriberId) {
-    //     const rawResponse = await fetch("http://localhost:7081/api/contacts/" + subscriberId, {method : "DELETE"})
-    //     const data = await rawResponse.json();
-    //     loadData();
-    //     // const newSubscribers = subscribersList.filter((subscriber) => subscriber.id !== subscriberId);
-    //     // setSubscribersList(newSubscribers);
-        
-    // }
-
-    const numberOfSubscribers = useMemo(()=>{
-        return subscribersList.length
-    }, [subscribersList])
+   
 
    async function addSubscriberHandler(newSubscriber) {
        const rawResponse = await fetch("http://localhost:7081/api/contacts", 
@@ -50,15 +43,6 @@ export default function PhoneDirectory() {
         const data = await rawResponse.json();
         loadData();
 
-        // if (subscribersList.length > 0) {
-        //     newSubscriber.id = subscribersList[subscribersList.length - 1].id + 1;
-        // }
-        // else {
-        //     newSubscriber.id = 1;
-        // }
-        // subscribersList.push(newSubscriber);
-        // setSubscribersList(subscribersList);
-
     }
 
     return (
@@ -68,7 +52,7 @@ export default function PhoneDirectory() {
                 <Route path='/add' element={<AddSubscriber addSubscriberHandler={(newSubscriber) => addSubscriberHandler(newSubscriber)} />} />
             </Routes>
         </Router>
-            <SubscriberCountContext.Provider value={numberOfSubscribers}>
+            <SubscriberCountContext.Provider value={state.count}>
                 <Footer />
             </SubscriberCountContext.Provider>
 
